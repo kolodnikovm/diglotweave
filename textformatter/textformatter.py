c644907ec07f9d0c9f5cbe864c90275a33ad9a6e
Kolodnikov Maxim <bredor419@gmail.com>
2017-12-17 16:12:42 +0700

    TextFormatter base functionality added

--- textformatter/textformatter.py
+++ textformatter/textformatter.py
@@ -1,30 +1,60 @@
-class TextFormatter:
-    def __init__(self, text, vocab):
-        self.text = text
-        self.vocab = vocab
-
-    def _getWord(text):
-        word = True
-        while word
-            word, text = text.split(" ", maxsplit=1)
-            yield word
-
-    def formatText(self):
-        for w in TextFormatter._getWord(self.text):
-            pass
-
+""" Module Docs """
+import re
+import csv
+import os.path
 import pymorphy2 as pm2
 
-text = """За запуском спутника из Центра управления полетами (ЦУП) Роскосмоса наблюдала представительная делегация
- — генеральный директор ракетно-космической корпорации «Энергия» Владимир Солнцев, ректор Томского политеха
- Пётр Чубик, директор ИФПМ СО РАН Сергей Псахье, советник генерального директора РКК «Энергия» Александр Чернявский,
- директор ИВФТ ТПУ Алексей Яковлев и другие. После того, как «Томск-ТПУ-120» вышел на самостоятельную орбиту,
- участники события, имеющие непосредственное отношение к рождению напечатанного 3D-наноспутника, поделились своими
- эмоциями и впечатлениями."""
+SCRIPT_PATH = os.path.dirname(__file__)
+FILE_NAME = os.path.join(SCRIPT_PATH, 'text.txt')
+VOCAB = {
+    "see": "видеть",
+    "it": "это",
+    "think": "думать"
+}
 
- vocab = {
- "satellite":"спутник",
- "councellor":"советник",
- "birth":"рождение",
- "share":"поделиться"
- }
+class TextFormatter:
+    """ TextFormatter documentation """
+
+    def __init__(self, text, vocab):
+        """ Init Docs """
+        self.text_file = open(text, "r", encoding='utf-8')
+        self.vocab = vocab
+        self.morph = pm2.MorphAnalyzer()
+
+    def get_text_words(self):
+        """ Возвращает первое слово из текстового файла класса в lower case"""
+        for line in self.text_file:
+            for word in re.findall(r'\w+', line):
+                yield word.lower()
+        self.text_file.close()
+
+    def get_eng_word(self, normal_word):
+        """ Возвращает английское слово из словаря, соовтетствующее данному переводу """
+        return list(self.vocab.keys())[list(self.vocab.values()).index(normal_word)]
+
+    def find_words_by_translate(self):
+        """ Находит все слова, перевод которых содержится в словаре """
+        result = {}
+        vocab_translates = list(map(str.lower, list(self.vocab.values())))
+        for word_from_text in self.get_text_words():
+            text_word_normal_form = self.morph.parse(word_from_text)[0].normal_form
+            if text_word_normal_form in vocab_translates:
+                eng_word = self.get_eng_word(text_word_normal_form) 
+                if eng_word in result.keys() and not word_from_text in result[eng_word]:
+                    result[eng_word].append(word_from_text)
+                else:
+                    result.update({eng_word:[word_from_text]})
+        return result
+
+    def save_to_csv(self):
+        """ Сохраняет результат работы функции  find_words_by_translate() в csv"""
+        with open(SCRIPT_PATH + '\words_to_swap.csv', 'w+', newline='', encoding='utf-8') as result_dict:
+            print(result_dict.name)
+            writer = csv.writer(result_dict)
+            words_to_swap = self.find_words_by_translate()
+            for eng_word in words_to_swap.keys():
+                for word in words_to_swap[eng_word]:
+                    writer.writerow([eng_word, word])
+
+TF = TextFormatter(FILE_NAME, VOCAB)
+TF.save_to_csv()
