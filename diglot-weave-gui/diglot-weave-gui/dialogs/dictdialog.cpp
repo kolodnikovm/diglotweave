16206f366385d71ac3b29a9fda48f0e8e8b8433d
thehemen <hemen4815@gmail.com>
2017-12-24 18:30:11 +0700

    implemented importing dictionary by txt

--- diglot-weave-gui/diglot-weave-gui/dialogs/dictdialog.cpp
+++ diglot-weave-gui/diglot-weave-gui/dialogs/dictdialog.cpp
@@ -8,21 +8,59 @@ DictDialog::DictDialog( QWidget * parent, UserAccount* user_account,
     this->formatted_text = formatted_text;
     this->usertextedit = usertextedit;
 
+    connect(ui.importDictButton, SIGNAL (released()), this, SLOT (import_dictionary()));
     connect(ui.addDictButton, SIGNAL (released()), this, SLOT (add_dictionary()));
     connect(ui.addWordButton, SIGNAL (released()), this, SLOT (add_word()));
     connect(ui.removeWordButton, SIGNAL(released()), this, SLOT (remove_word()));
     connect(ui.closeDictButton, SIGNAL (released()), this, SLOT (close()));
 
-    connect(ui.dictList, SIGNAL (itemSelectionChanged()), this, SLOT (rewrite_table()));
+    connect(ui.dictList, SIGNAL (itemSelectionChanged()), this, SLOT (rewrite_word_table()));
 
-    this->rewrite_list();
+    this->rewrite_dict_list();
+}
+
+void DictDialog::import_dictionary()
+{
+    QString file_name = QFileDialog::getOpenFileName(this, "Открыть словарь", "/home/", "Text Files (*.txt);All Files (*)");
+    QFile f(file_name);
+
+    if(!f.open(QIODevice::ReadOnly)) {
+        return utils::show_error("Произошла ошибка при попытке открытия файла.");
+    }
+
+    QFileInfo file_info(f.fileName());
+    QString file_name_path(file_info.fileName());
+    QStringList file_name_listed = file_name_path.split('.');
+    QString file_name_only = file_name_listed[0];
+
+    this->user_account->CreateDictionary(file_name_only);
+    int dict_index = this->user_account->GetDictionariesCount() - 1;
+
+    QTextStream in(&f);
+    while(!in.atEnd())
+    {
+        QString word_translate_line;
+        in >> word_translate_line;
+        QStringList word_translate = word_translate_line.split(',');
+
+        if(word_translate.size() == 2)
+        {
+            QString word_value = word_translate[0];
+            QString translate_value = word_translate[1];
+            this->user_account->GetDictionary(dict_index)->add_word(word_value, translate_value);
+        }
+    }
+
+    f.close();
+
+    this->rewrite_dict_list();
 }
 
 void DictDialog::add_dictionary()
 {
     QString dict_value = ui.dictNameEdit->text();
     this->user_account->CreateDictionary(dict_value);
-    this->rewrite_list();
+    this->rewrite_dict_list();
 }
 
 void DictDialog::add_word()
@@ -34,7 +72,7 @@ void DictDialog::add_word()
     if(dict_index >= 0 && dict_index < this->user_account->GetDictionariesCount())
     {
         this->user_account->GetDictionary(dict_index)->add_word(word_value, translate_value);
-        this->rewrite_table();
+        this->rewrite_word_table();
     }
 }
 
@@ -47,17 +85,23 @@ void DictDialog::remove_word()
     QString translate = ui.dictContentTable->item(current_row, 1)->text();
 
     this->user_account->GetDictionary(dict_index)->remove_word(word, translate);
-    this->rewrite_table();
+    this->rewrite_word_table();
 }
 
-void DictDialog::rewrite_list()
+void DictDialog::rewrite_dict_list()
 {
+    int dict_count = this->user_account->GetDictionariesCount();
+
     ui.dictList->clear();
-    for(int i = 0; i < this->user_account->GetDictionariesCount(); ++i)
-        ui.dictList->addItem(this->user_account->GetDictionary(i)->dict_name);
+
+    for(int dict_index = 0; dict_index < dict_count; ++dict_index)
+    {
+        QString dict_name = this->user_account->GetDictionary(dict_index)->dict_name;
+        ui.dictList->addItem(dict_name);
+    }
 }
 
-void DictDialog::rewrite_table()
+void DictDialog::rewrite_word_table()
 {
     int dict_index = ui.dictList->currentRow();
 
